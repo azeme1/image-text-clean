29fc39a038b47395c363ecb61fb4936cc95d913e
Alexander Lemeza <alexander.lemeza@oxagile.com>
2024-04-27 19:08:27 +0200

    [~] Upgrade to text filtering and inpainting

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import argparse
 import os
 
+import ast
 import cv2
 import numpy as np
 from glob import glob
 from tqdm import tqdm
 import onnxruntime as rt
+import easyocr
 
 import craft_utils
 
@@ -46,7 +48,8 @@ def process_image(frame_data, inference_size=1280):
     M_document_craft, _ = cv2.findHomography(p_frame_src, p_document_dst)
     shape_document_craft_dst = (inference_size * normalized_shape).astype(np.int32)
 
-    frame_data_inference = cv2.warpPerspective(frame_data, M_document_craft, tuple(reversed(shape_document_craft_dst)))
+    frame_data_inference = cv2.warpPerspective(frame_data, M_document_craft, 
+                                               tuple(reversed(shape_document_craft_dst)))
 
     y = detect_text(frame_data_inference)
     score_text = y[0, 0]
@@ -76,17 +79,36 @@ def process_image(frame_data, inference_size=1280):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Remove Text From Image")
-    parser.add_argument("--src", default="./data/data_src/", type=str, help="Image folder source")
-    parser.add_argument("--dst", default="./data/data_dst/", type=str, help="Image folder destination")
+    parser.add_argument("--src", default="./data/data_src/", type=str,
+                        help="Image folder source")
+    parser.add_argument("--dst", default="./data/data_dst/", type=str,
+                        help="Image folder destination")
+    parser.add_argument("--ki", default=(5, 3), type=tuple,
+                        help="Enlarge mask rule (K, I): K for I iterations")
+    parser.add_argument("--exclude_patterns_path", default="./data/exclude_pattern.txt",
+                        help="Path to text patterns for exclude")
 
     args = parser.parse_args()
     folder_src = args.src
     folder_dst = args.dst
+    ksize, iterations = args.ki
+    exclude_patterns_path = args.exclude_patterns_path
+    model_q_path_onnx = "./data/models/places2.pth_Q.onnx"
+
+    if os.path.isfile(exclude_patterns_path):
+        with open(exclude_patterns_path, 'r') as f:
+            exclude_patterns = ast.literal_eval(''.join(f.readlines()))
+    else:
+        exclude_patterns = []
 
     extension_list = ['png', 'jpg', 'jpeg']
     file_list_src = glob(os.path.join(folder_src, '', '**', '*.*')) + glob(os.path.join(folder_src, '', '*.*'))
     file_list_src = [item for item in file_list_src if item.split('.')[-1].lower() in extension_list]
     file_list_dst = [item.replace('\\', '/').replace(folder_src, folder_dst) for item in file_list_src]
+
+    reader = easyocr.Reader(['en'])
+    model_inpaint = rt.InferenceSession(model_q_path_onnx)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (ksize, ksize))
 
     for path_src, path_dst in tqdm(zip(file_list_src, file_list_dst), total=len(file_list_dst)):
         assert os.path.abspath(path_src) != os.path.abspath(path_dst)
@@ -97,8 +119,25 @@ if __name__ == '__main__':
 
         frame_src = np.ascontiguousarray(frame_src[..., :3])
 
-        frame_dst, _ = process_image(frame_src)
+        ocr_result = reader.readtext(path_src)
+
+        mask_src = np.full_like(frame_src, 255, dtype=np.uint8)
+        for shape, text, confidence in ocr_result:
+            if any([pattern in text.lower() for pattern in exclude_patterns]):
+                continue
+            cv2.fillPoly(mask_src, [np.array(shape, dtype=np.int32)], 0)
+
+        frame_in = cv2.resize(frame_src, (512, 512))
+        mask_in = cv2.resize(mask_src, (512, 512), interpolation=cv2.INTER_NEAREST)
+        mask_in = cv2.erode(mask_in, kernel, iterations=iterations)
+        frame_out = model_inpaint.run(None, {'frame': frame_in, 'mask': mask_in})[0][0]
+
+        frame_out = cv2.resize(frame_out, tuple(reversed(frame_src.shape[:2])))
+        alpha = cv2.resize(mask_in, tuple(reversed(frame_src.shape[:2]))) / 255.
+
+        frame_result = (frame_src * alpha + (1 - alpha) * frame_out).astype(np.uint8)
+
+        # frame_show = np.hstack([frame_src, frame_result])
 
         os.makedirs(os.path.dirname(path_dst), exist_ok=True)
-
-        cv2.imwrite(path_dst, frame_dst)
+        cv2.imwrite(path_dst, frame_result)
